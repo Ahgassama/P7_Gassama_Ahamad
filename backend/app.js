239951b32a5a5225aa3f38c0ca0d4596e36546c2
Ahgassama <ahmad.gassama@gmail.com>
2022-01-26 14:16:47 +0100

    installation dépendances sécu

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mysql = require("mysql2");
 const app = express();
+require("dotenv").config({ path: "./env" });
 
 app.use((req, res) => {
   res.json({ message: "Votre requête a bien été reçue !" });
 });
-
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
