722117552d962250be181e80e737a54341e3423b
Ahgassama <ahmad.gassama@gmail.com>
2022-01-26 13:36:00 +0100

    config dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const app = require("./app");
+require("dotenv").config({ path: "./env" });
 
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
@@ -12,7 +13,7 @@ const normalizePort = (val) => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT);
 app.set("port", port);
 
 const errorHandler = (error) => {
