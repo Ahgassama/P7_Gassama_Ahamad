9c274c77456bac7f2b979e0b41290bdd96759996
Ahgassama <ahmad.gassama@gmail.com>
2022-02-12 19:19:34 +0100

    usage du useState pour la page login

--- frontend/src/components/Log/index.js
+++ frontend/src/components/Log/index.js
@@ -1,13 +1,31 @@
-import React from "react";
-
+import React, { useState } from "react";
+import SignUpForm from "./SignupForm";
+import LoginForm from "./LoginForm";
 const Log = () => {
+  const [signUpModal, setSignupModal] = useState(true);
+  const [loginModal, setloginModal] = useState(false);
+  const handleModals = (e) => {
+    if (e.target.id === "register") {
+      setloginModal(false);
+      setSignupModal(true);
+    } else if (e.target.id === "login") {
+      setSignupModal(false);
+      setloginModal(true);
+    }
+  };
   return (
     <div class="connection-form">
       <div class="log-container">
         <ul>
-          <li>Inscription</li>
-          <li>Connexion</li>
+          <li onClick={handleModals} id="register">
+            Inscription
+          </li>
+          <li onClick={handleModals} id="login">
+            Connexion
+          </li>
         </ul>
+        {signUpModal && <SignUpForm />}
+        {loginModal && <LoginForm />}
       </div>
     </div>
   );
