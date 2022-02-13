83de7d76cec5eb54d22ab06c2ae558f73c647526
Ahgassama <ahmad.gassama@gmail.com>
2022-02-13 18:06:51 +0100

    ajout cors backend

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,22 @@
 const express = require("express");
-////const mysql = require("mysql2");
+
 const app = express();
 const userRoutes = require("./routes/user.route");
 console.log(userRoutes);
-//require("dotenv").config();
-//require();
-//console.log(process.env);
-/*app.use((req, res) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-});*/
-/*const connection = mysql.createConnection({
-  host: process.env.DBHOST,
-  user: process.env.DBUSER,
-  database: process.env.DATABASE,
-  password: process.env.DBPASSWORD,
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
-connection.query("SELECT * FROM `Users`", function (err, results, fields) {
-  console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra meta data about results, if available
-});*/
-//app.get("/", res.status(200));
+
 app.use(express.json());
 app.use("/api/user", userRoutes);
 module.exports = app;
