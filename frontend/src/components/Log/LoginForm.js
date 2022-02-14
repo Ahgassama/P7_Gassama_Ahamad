f2937921de4197330d01a4b0a6a50e424566a451
Ahgassama <ahmad.gassama@gmail.com>
2022-02-14 11:30:41 +0100

    exp méthode post sur formulaire (login)

--- frontend/src/components/Log/LoginForm.js
+++ frontend/src/components/Log/LoginForm.js
@@ -1,7 +1,64 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 const LoginForm = () => {
-  return <div>Se connecter</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    const emailError = document.querySelector(".email-error");
+    const passwordError = document.querySelector(".password-error");
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url: `http://localhost:3000/api/user/login`,
+      withCredentials: true,
+      data: {
+        email,
+        password,
+      },
+    })
+      .then((res) => {
+        if (res.data.errors) {
+          emailError.innerHTML = res.data.errors.email;
+          passwordError.innerHTML = res.data.errors.password;
+        } else {
+          window.location = "/";
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div>
+      <form action="" onSubmit={handleLogin} id="login-form">
+        <label htmlFor="email">E-mail</label>
+        <br />
+        <input
+          type="text"
+          name="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+        <div className="email-error"></div>
+        <br />
+        <label htmlFor="text">Mot de passe</label>
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+        <div className="password-error"></div>
+        <br />
+        <input type="submit" value="Se connecter" />
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
--- frontend/src/components/Log/SignupForm.js
+++ frontend/src/components/Log/SignupForm.js
@@ -1,7 +1,18 @@
 import React from "react";
+import axios from "axios";
 
 const SignUpForm = () => {
-  return <div>Inscription</div>;
+  return (
+    <div>
+      Inscription
+      <form>
+        <input type="text" email="email" id="email" required />
+        <input type="text" name="name" id="name" required />
+        <input type="text" surname="surname" id="surname" required />
+        <input type="text" password="password" id="password" required />
+      </form>
+    </div>
+  );
 };
 
 export default SignUpForm;
