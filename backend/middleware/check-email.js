ef73411d328ff55bf46fb021dbe3aaa159968fbd
Ahgassama <ahmad.gassama@gmail.com>
2022-02-18 20:30:34 +0100

    modele regex

---
+++ backend/middleware/check-email.js
@@ -0,0 +1,10 @@
+module.exports = (req, res, next) => {
+  const trueEmail = (email) => {
+    let emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    let regexTrue = emailRegex.test(email);
+    regexTrue
+      ? next()
+      : res.status(400).json({ message: "adresse mail non valide" });
+  };
+  trueEmail(req.body.email);
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/user.controller");
+const checkEmail = require("../middleware/check-email");
 
-router.post("/signup", userCtrl.create);
+router.post("/signup", checkEmail, userCtrl.create);
 router.post("/login", userCtrl.login);
 router.delete("/:id", userCtrl.delete);
 router.put("/:id", userCtrl.update);
