eb1046a1f5a381cb1d28d523486e6de762dc294e
Ahgassama <ahmad.gassama@gmail.com>
2022-03-23 21:28:28 +0100

    ajout logo feed

--- frontend/src/components/Header/HeaderProfile.js
+++ frontend/src/components/Header/HeaderProfile.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import axios from "axios";
 import Logout from "../Log/Logout";
 import "./header.scss";
@@ -30,6 +31,9 @@ const HeaderProfile = () => {
 
       <div className="logo-profil">
         <Logout />
+        <NavLink exact="true" to="/">
+          <img src="./images/feed.png" alt="profile" />
+        </NavLink>
       </div>
     </header>
   );
