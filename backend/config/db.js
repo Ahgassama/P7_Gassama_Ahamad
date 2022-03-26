424e92d86c682df8382d9d0c5e069af7d41b8be3
Ahgassama <ahmad.gassama@gmail.com>
2022-03-26 16:01:57 +0100

    commentaires

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,34 +12,4 @@ connection.connect((error) => {
   console.log("Successfully connected to the database.");
 });
 
-/*connection.query(
-  "INSERT INTO Users SET ?",
-  {
-    name: "name",
-    surname: "surname",
-    email: "email",
-    isAdmin: false,
-  },
-  function (error, results, fields) {
-    if (error) throw error;
-    console.log(results.insertId);
-  }
-);*/
-/*const User = function (User) {
-  this.name = User.name;
-  this.surname = User.surname;
-  this.email = User.email;
-  this.isAdmin = User.isAdmin;
-  this.password = User.password;
-};
-connection.query("INSERT INTO `Users` SET ?", User, (err, res) => {
-  if (err) {
-    console.log("error: ", err);
-    result(err, null);
-    return;
-  }
-  console.log("created Users: ", { id: res.insertId, ...User });
-  result(null, { id: res.insertId, ...newUser });
-});*/
-
 module.exports = connection;
